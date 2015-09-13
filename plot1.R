a0ac4f47b3075198819bf80765f2b2079977626c
wnpChancellor <wnpChancellor@users.noreply.github.com>
2015-09-12 20:47:34 -0400

    R code for Plot1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+# Read in data #
+data<-read.csv("/Users/jingshi/desktop/Coursera/household_power_consumption.txt",header=TRUE,sep=";",dec=".",na.strings="?")
+
+#Subset #
+datasub<-subset(data,Date=="1/2/2007" | Date=="2/2/2007")
+
+#Create and format weekday variable#
+datasub$day<-paste(datasub$Date,datasub$Time, sep=" ")
+datasub$day<-as.POSIXct(datasub$day,format="%d/%m/%Y %H:%M:%S")
+
+#Plot1 and save as PNG#
+png("/Users/jingshi/desktop/Coursera/plot1.png",width=480,height=480)
+hist(datasub$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (Kilowatts)")
+dev.off()
